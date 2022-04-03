0bf49e80605ec72be16c51aa36e9564386a1a9e5
NyanPham <phamthanhnhanussh@gmail.com>
2022-04-03 17:42:14 +0700

    working on grid cell

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import BootsOnRock from '../assets/boots-on-rock.jpg'
+import BootMountain from '../assets/boot-mountain.jpg'
+import KidHiking from '../assets/kid-hiking.jpg'
+import HikingStep from '../assets/hiking-zoom.jpg'
+import ImageGridCell from './ImageGridCell'
 
 export default function ImageGrid() {
     return (
-        <div className="grid grid-cols-4 grid-rows-2 w-full h-96">
-            <div className="col-span-2 row-span-2 bg-gray-500"></div>
-            <div className="row-span-2 bg-green-300"></div>
-            <div className="bg-sky-900"></div>
-            <div className="bg-gray-500"></div>
+        <div className="grid grid-cols-4 grid-rows-2 w-full h-96 gap-3 mt-3">
+            <div className="col-span-2 row-span-2 bg-gray-500">
+                <ImageGridCell image={BootsOnRock} title={"Powerful"} description={'Our boots will boost your power on uneven rocks'}/>
+            </div>
+            <div className="row-span-2 bg-green-300">
+                <ImageGridCell image={BootMountain} title={"Stiff"} description={'Our boots will boost your power on uneven rocks'}/>
+            </div>
+            <div className="bg-sky-900">
+                <ImageGridCell image={KidHiking} title={"Mature"} description={'Even kids can join your journey'}/>
+            </div>
+            <div className="bg-gray-500">
+                <ImageGridCell image={HikingStep} title={"Charming"} description={'We will cover your every step'}/>
+            </div>
         </div>
     )
 }
--- src/components/ImageGridCell.js
+++ src/components/ImageGridCell.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function ImageGridCell({image, title, description}) {
     return (
-        <div className='relative w-full h-full'>
-            <img src={image} alt={title} className="absolute inset-0 object-cover" />
+        <div className='relative w-full h-full py-3 px-5 z-10'>
+            <img src={image} alt={title} className="absolute inset-0 object-cover object-bottom w-full h-full -z-10" />
+            <h3 className="text-xl text-emerald-500 italic font-semibold">{title}</h3>
+            <p className="text-2xl text-white font-semibold italic">{description}</p>
         </div>
     )
 }
